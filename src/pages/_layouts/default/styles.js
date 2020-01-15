079b87329dd09269049afea4153c83b7824b266a
Lucas Calixto <lucas.calixto@eduzz.com>
2020-01-14 23:11:46 -0300

    Commit Inicial

---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  overflow: auto;
+  background: linear-gradient(-90deg, #3b9eff, #36d1d6);
+  background: linear-gradient(-90deg, #7159c1, #ab59c1);
+`;
